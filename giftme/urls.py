de7854133354c2abc9185c290d590a239d86ea5a
timcallagy <timcallagy@gmail.com>
2015-03-17 22:12:31 +0000

    Added get_gifts method

--- giftme/urls.py
+++ giftme/urls.py
@@ -4,4 +4,5 @@ from giftme import views
 urlpatterns = patterns('',
         url(r'^add_gift/$', views.add_gift),
         url(r'^get_csrf_token/$', views.get_csrf_token),
+        url(r'^get_gifts/(?P<id>[0-9]+)/$', views.get_gifts),
 )
--- giftme/views.py
+++ giftme/views.py
@@ -1,8 +1,12 @@
 from giftme.forms import GiftForm
+from giftme.models import Gift
 from django.template import RequestContext
 from django.http import HttpResponse
 from django.middleware.csrf import _get_new_csrf_key as get_new_csrf_key
 from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
+import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 def get_csrf_token(request):
     """
@@ -20,12 +24,16 @@ def add_gift(request):
     context = RequestContext(request)
     if request.method == 'POST':
         add_gift_form = GiftForm(data=request.POST)
-        print(add_gift_form)
         if add_gift_form.is_valid():
             gift = add_gift_form.save()
             return HttpResponse('true')
         else:
-            print('invalid')
             return HttpResponse('false')
     else:
         return HttpResponse('false')
+
+def get_gifts(request, id):
+    gifts = Gift.objects.filter(owner_id = id );
+    data = serializers.serialize('json', gifts)
+    return HttpResponse(data)
+
