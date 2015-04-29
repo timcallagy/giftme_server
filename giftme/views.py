9cfcfd683b267d91a7c83950c4602c3d3c2a2784
timcallagy <timcallagy@gmail.com>
2015-04-29 08:12:02 +0100

    Updated supported versions list

--- giftme/views.py
+++ giftme/views.py
@@ -53,7 +53,7 @@ def login(request):
 
 @csrf_exempt
 def wakeup(request):
-    supportedVersions = ['0.0.22', '0.0.23']
+    supportedVersions = ['0.0.23', '0.0.24']
     logger = logging.getLogger('giftme')
     logger.debug('In wakeup function.')
     if request.method == 'POST':
